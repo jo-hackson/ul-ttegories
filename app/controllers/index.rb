601d2490f96720362d8fa9915d587f287c9e0404
Joanna <joanna.yeh.jackson@gmail.com>
2017-08-04 07:21:41 -0500

    Trying to display results dynamically

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,3 +9,23 @@ get '/results' do
   # @categories = Category.get_categories
   erb :results
 end
+
+post '/results' do
+
+	content_type :json
+	return {
+		1 => [params["1"], "+1"],
+		2 => [params["2"], "+2"],
+		3 => [params["3"], "+3"],
+		4 => [params["4"], "+4"],
+		5 => [params["5"], "+1"],
+		6 => [params["6"], "+1"],
+		7 => [params["7"], "+1"],
+		8 => [params["8"], "+1"],
+		9 => [params["9"], "+1"],
+		10 => [params["10"], "+1"]
+	}.to_json
+
+
+end
+
